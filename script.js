74db480cdb97a45b7ccfc825ce0c119725c901cb
panditkrishna5658-alt <panditkrishna5658@gmail.com>
2026-05-20 18:56:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+body{
+  margin:0;
+  font-family:Arial;
+  background:#111;
+  color:white;
+}
+
+header{
+  background:#0f5132;
+  text-align:center;
+  padding:30px;
+}
+
+.hero{
+  text-align:center;
+  padding:80px 20px;
+}
+
+.btn{
+  display:inline-block;
+  margin-top:20px;
+  padding:12px 25px;
+  background:#198754;
+  color:white;
+  text-decoration:none;
+  border-radius:10px;
+}
+
+.products{
+  padding:50px 20px;
+  text-align:center;
+}
+
+.card{
+  background:#222;
+  margin:20px auto;
+  padding:20px;
+  max-width:400px;
+  border-radius:15px;
+}
+
+.contact{
+  text-align:center;
+  padding:50px 20px;
+  background:#1a1a1a;
+}
+
+footer{
+  text-align:center;
+  padding:20px;
+  background:black;
+}
